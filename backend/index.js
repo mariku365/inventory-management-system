212a37426226e3a1274779ecae50b8ac478a4797
m4rk64rci4-netizen <m4rk64rci4@gmail.com>
2026-08-20 03:57:30 +0800

    Feat: Added items router for CRUD operations

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const { connectDB } = require('./dbConnection');
+const itemsRouter = require('./routes/items');
 
 const app = express();
 app.use(express.json());
@@ -9,3 +10,6 @@ app.use(express.json());
 connectDB();
 
 app.listen(process.env.DB_PORT, () => console.log('Backend is listening in port', process.env.DB_PORT));
+
+
+app.use('/items', itemsRouter);
---
+++ backend/routes/items.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const { sql } = require("../dbConnection");
+
+// CREATE OPERATION
+router.post('/', async (req, res) => {
+    const {itemName, quantity, price} = req.body;
+    try {
+        await sql.query(`INSERT INTO items(itemName, quantity, price) VALUES(${itemName}, ${quantity}, ${price})`);
+        res.status(201).send('Item added successfully!');
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
+})
+
+// READ OPERATION
+router.get('/', async (req, res) => {
+    try {
+        const result = await sql.query("SELECT * FROM items");
+        res.json(result.recordset);
+    } catch (err){
+        res.status(500).send(err.message)
+    }
+});
+
+// UPDATE OPERATION
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const {itemName, quantity, price} = req.body;
+    try {
+        await sql.query(`UPDATE items(itemName, quantity, price) SET itemName = ${itemName}, quantity = ${quantity}, price = ${price} WHERE id = ${id}`);
+        res.send('Item updated successfully!');
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
+})
+
+// DELETE OPERATION
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        await sql.query(`DELETE * FROM items WHERE id = ${id}`);
+        res.send('Item deleted successfully!');
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+
+})
+
+module.exports = router;
